f9945549270d0e1c9838060b4bf26499b629e439
Kevingu08 <guidourbinakevin@gmail.com>
2023-10-20 00:39:46 -0600

    footer: estructura cambiada. fuente hindu añadida

--- js/main.js
+++ js/main.js
@@ -1,61 +1,63 @@
 function init(){
-    
-    var swiper = new Swiper('.swiper', {
-        loop: true,
-        autoplay: true,
-
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-        },
-        
-        // init: false,
-        pagination: {
-          el: '.swiper-pagination',
-          clickable: true,
-        },
-        
-        slidesPerView: 1,
-        spaceBetween: 40,
-      
-        breakpoints: {
-          620: {
-            slidesPerView: 1,
-            spaceBetween: 20,
-          },
-          680: {
-            slidesPerView: 2,
-            spaceBetween: 40,
-          },
-          920: {
-            slidesPerView: 3,
-            spaceBetween: 40,
-          },
-          1240: {
-            slidesPerView: 3,
-            spaceBetween: 50,
-          },
-        } 
-        });
+  adaptiveMenu();
+  initSlider();
 }
 
+// funcion para inicializar el slider de swiper
+function initSlider(){
+  var swiper = new Swiper('.swiper', {
+    loop: true,
+    autoplay: true,
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    
+    slidesPerView: 1,
+    spaceBetween: 40,
+  
+    breakpoints: {
+      620: {
+        slidesPerView: 1,
+        spaceBetween: 20,
+      },
+      680: {
+        slidesPerView: 2,
+        spaceBetween: 40,
+      },
+      920: {
+        slidesPerView: 3,
+        spaceBetween: 40,
+      },
+      1240: {
+        slidesPerView: 3,
+        spaceBetween: 50,
+      },
+    } 
+    });
+}
+
+// funcion para adaptar el menu en las diferentes pantallas de movil
 function adaptiveMenu(){
   let topNav = document.getElementById("top-navigation");
   let navList = document.getElementById("navigation-list");
   let mobileCheck = document.getElementById("mobile-check");
   let height = topNav.offsetHeight;
+
   mobileCheck.addEventListener("click",function(){
     if(this.checked){
       console.log(height);
       navList.style.top = height + "px";
     }
   });
-
-  
 }
 
 document.addEventListener("DOMContentLoaded", function(){
-    adaptiveMenu();
     init()
-    
 });
